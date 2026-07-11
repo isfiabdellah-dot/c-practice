3ab68608085991e5d89542c879f6f7c69f8e7249
isfiabdellah-dot <isfiabdellah@gmail.com>
2026-07-11 17:06:08 +0100

    finishing ex1 LEVEL1

---
+++ level1/ex1.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main(){
+
+int x = 25;
+int *ptr;
+ptr = &x;
+
+printf("addresse of x from &x : %p\n",&x);
+printf("addresse of x from ptr : %p\n",ptr);
+
+printf("value of x from &x : %d\n", x);
+printf("value of x from *ptr : %d\n",*ptr);
+
+*ptr=99;
+printf("value of x after changing value : %d\n", x);
+}
