e77aa00ff952380ad0e318cb138a97b88e3732c9
isfiabdellah-dot <isfiabdellah@gmail.com>
2026-07-11 17:31:19 +0100

    finishing ex2 LEVEL1

---
+++ level1/ex2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    double pi = 3.14159;
+    double *pPi = &pi;
+    
+    printf("size of pi %d\n", sizeof(pi));
+    printf("size of pPi %d\n", sizeof(pPi));
+    printf("size of data pPi points to :  %d\n", sizeof(*pPi));
+
+    //The pointers always have 8 bytes size on a 64-bit system in and 4 32-bit system, 
+    //which is the size of the address he's holding, 
+    //whatever the type he is pointing to.
+}
